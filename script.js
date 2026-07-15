07dabef5739d3f3193f10549ac2d4a0db4304f66
renan033 <renanpires26082003@gmail.com>
2026-07-14 21:49:35 -0300

    Adicionando contador regressivo

---
+++ script.js
@@ -0,0 +1,20 @@
+const dataCasamento = new Date("October 3, 2026 17:30:00").getTime();
+
+const contador = setInterval(() => {
+
+  const agora = new Date().getTime();
+  const distancia = dataCasamento - agora;
+
+  const dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+  const horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+  const segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+
+  document.getElementById("dias").innerHTML = dias;
+  document.getElementById("horas").innerHTML = horas;
+  document.getElementById("minutos").innerHTML = minutos;
+  document.getElementById("segundos").innerHTML = segundos;
+
+}, 1000);
+
+
