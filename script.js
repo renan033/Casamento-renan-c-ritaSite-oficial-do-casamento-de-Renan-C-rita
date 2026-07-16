536b8c40da5c0e6cdf4788d4c64bced2418879c0
renan033 <renanpires26082003@gmail.com>
2026-07-15 23:29:17 -0300

    atualizar contador

--- script.js
+++ script.js
@@ -2,34 +2,38 @@
 // CONTADOR REGRESSIVO
 // ===============================
 
-const dataCasamento = new Date("2026-10-03T17:00:00").getTime();
+Pconst dataCasamento = new Date(2026, 9, 3, 17, 0, 0); // 03/10/2026 às 17:00
 
 function atualizarContador() {
-const agora = new Date().getTime();
-const diferenca = dataCasamento - agora;
 
-if (diferenca <= 0) {
-    document.getElementById("dias").textContent = "00";
-    document.getElementById("horas").textContent = "00";
-    document.getElementById("minutos").textContent = "00";
-    document.getElementById("segundos").textContent = "00";
-    return;
-}
+    const agora = new Date();
 
-const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
-const horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
-const segundos = Math.floor((diferenca % (1000 * 60)) / 1000);
+    const diferenca = dataCasamento - agora;
 
-document.getElementById("dias").textContent = dias;
-document.getElementById("horas").textContent = horas;
-document.getElementById("minutos").textContent = minutos;
-document.getElementById("segundos").textContent = segundos;
+    if (diferenca <= 0) {
 
+        document.getElementById("dias").textContent = "00";
+        document.getElementById("horas").textContent = "00";
+        document.getElementById("minutos").textContent = "00";
+        document.getElementById("segundos").textContent = "00";
+
+        return;
+    }
+
+    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferenca / (1000 * 60 * 60)) % 24);
+    const minutos = Math.floor((diferenca / (1000 * 60)) % 60);
+    const segundos = Math.floor((diferenca / 1000) % 60);
+
+    document.getElementById("dias").textContent = dias;
+    document.getElementById("horas").textContent = horas;
+    document.getElementById("minutos").textContent = minutos;
+    document.getElementById("segundos").textContent = segundos;
 }
 
 atualizarContador();
 setInterval(atualizarContador, 1000);
+
 function copiarPix(){
 
   function copiarPix() {
